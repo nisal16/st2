22e7b198ca00e2998247b7443160ecf5ed45ce7e
Nisal De Silva <Nisal De Silva@DESKTOP-MSICX626QD>
2017-04-08 12:21:23 +0530

    print3

--- Test/src/test/Test.java
+++ Test/src/test/Test.java
@@ -17,6 +17,8 @@ public class Test {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello world");
+         System.out.println("hello world");
+          System.out.println("hello world");
     }
     
 }
